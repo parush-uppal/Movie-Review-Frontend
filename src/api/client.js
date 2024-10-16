0dbbbe5eabdbb01dfbc81407fe0262b2a9ebc375
Parush Uppal <114945355+parush-uppal@users.noreply.github.com>
2024-10-16 06:50:42 +0000

    Changed backend route

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "https://cinema-opus.onrender.com/api" });
+const client = axios.create({ baseURL: "https://e4s1kmrip8.execute-api.ap-south-1.amazonaws.com/prod/api" });
+// const client = axios.create({ baseURL: "https://cinema-opus.onrender.com/api" });
 // const client = axios.create({ baseURL: "http://127.0.0.1:8000/api" });
 
 export default client;
